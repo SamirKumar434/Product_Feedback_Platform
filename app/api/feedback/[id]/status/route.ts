b89f9b11bd5648e759e1bf91fd04548ac395f73e
Samir Kumar <samsahu545@gmail.com>
2026-08-30 00:02:01 +0530

    updated id feedback routes.ts

---
+++ app/api/feedback/[id]/status/route.ts
@@ -0,0 +1,52 @@
+import { STATUS_ORDER } from "@/app/data/status-data";
+import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: number }> },
+) {
+  try {
+    const { userId } = await auth();
+
+    if (!userId) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+    const user = await prisma.user.findUnique({
+      where: {
+        clerkUserId: userId,
+      },
+    });
+    if (!user || user.role !== "ADMIN") {
+      return NextResponse.json(
+        { error: "Admin access required" },
+        { status: 403 },
+      );
+    }
+    const { status } = await request.json();
+    const { id: postId } = await params; // Validate status
+    if (!STATUS_ORDER.includes(status)) {
+      return NextResponse.json({ error: "Invalid status" }, { status: 400 });
+    }
+
+    const updatePost = await prisma.post.update({
+      where: { id: postId },
+      data: {
+        status,
+      },
+      include: {
+        author: true,
+        votes: true,
+      },
+    });
+    return NextResponse.json(updatePost);
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error: "Internal server error",
+      },
+      { status: 500 },
+    );
+  }
+}
