370acd81add3d8b52b2ce7c187104c9ec225584c
Samir Kumar <samsahu545@gmail.com>
2026-08-30 14:55:45 +0530

    changes made feedback page.tsx

---
+++ components/feedback-list.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export default function FeedbackList({
+  initialPosts,
+  userId,
+}: {
+  initialPosts: any[];
+  userId: string | null;
+}) {
+  return <>FeedbackList</>;
+}
